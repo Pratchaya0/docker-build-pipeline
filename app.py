cc16dcb8d3dc5fd293c8d17c50202acd3a08530d
Pratchaya0 <pratchayaworks@gmail.com>
2026-04-28 13:20:27 +0700

    Initialize Flask application with Docker support and CI/CD pipeline. Added .dockerignore, .gitignore, Dockerfile, requirements.txt, and GitHub Actions workflow for building and pushing to GHCR.

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.get("/")
+def home():
+    return "GHCR pipeline test is working!\n"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8000)
